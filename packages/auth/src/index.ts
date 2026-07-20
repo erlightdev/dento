fcdeec118c071578e4175ac369bc2f4ff5de7788
erlightdev <lightdeveloper@protonmail.com>
2026-07-20 16:26:49 +0545

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,32 @@
+import { createDb } from "@Dento/db";
+import * as schema from "@Dento/db/schema/auth";
+import { env } from "@Dento/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "mysql",
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
---
+++ packages/db/src/index.ts
@@ -0,0 +1,16 @@
+import { env } from "@Dento/env/server";
+import { drizzle } from "drizzle-orm/mysql2";
+
+import * as schema from "./schema";
+
+export function createDb() {
+  return drizzle({
+    connection: {
+      uri: env.DATABASE_URL,
+    },
+    schema,
+    mode: "default",
+  });
+}
+
+export const db = createDb();
